7c79565d166f6407171cc5cb9c0f970d8c444799
Maksim Ivliev <maxim110597@yandex.ru>
2023-01-02 22:51:34 +0300

    Доработать проект

--- Project_1/MainWindow.xaml.cs
+++ Project_1/MainWindow.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using System.Windows;
 
-namespace Project_1
+namespace Project_1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
